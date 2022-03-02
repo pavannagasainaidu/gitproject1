7ec43439b7fe31f673757bf845594183e8e7b7f3
pavan <pavan@Pavan>
2022-03-02 10:28:45 +0530

    first commit

--- src/gitdemoproject1/Static.java
+++ src/gitdemoproject1/Static.java
@@ -1,5 +1,29 @@
 package gitdemoproject1;
+chkstatic
+{
+	 static int a=0;
+	 static
+	 {
+		 System.out.println("I m a static block");
+	 }
+	 chkstatic()
+	 {}
+		 static void disp()
+		 {
+			 a++;
+			 System.out.println(a);
+		 }
+	 }
 
-public class Static {
-
+public class Static{
+	 static
+	 {
+		 System.out.println("Static block from main");
+	 }
+	 System.out.println("i am an instance block");
+public static void main(String[] args) {
+	 System.out.println("i am main method");
+	 Static se=new Static();
+	 for(int i=0;i<5;i++);
+}
 }
